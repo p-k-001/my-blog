348bbee569cb0a4ccafd4bce8583638419489753
pk <pavel_kohout@centrum.cz>
2025-01-28 14:30:14 +0100

    tst

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  devToolbar: { enabled: false },
+  site: "https://example.com",
+  integrations: [preact()],
+});
